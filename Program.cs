01d6f8ee3563e5ed33ef349b7e4f8d161da4ede4
Sins_621 <sinsmailza@gmail.com>
2025-04-18 21:04:00 +0200

    refactor/switch-to-VS-console-app

--- Program.cs
+++ Program.cs
@@ -1,12 +1,51 @@
-﻿string password = "Password";
+﻿//using System.Runtime.CompilerServices;
 
-Dictionary<string, string> special_chars = new Dictionary<string, string>()
+//string password = "password";
+
+//Dictionary<char, List<char>> leetMap = new Dictionary<char, List<char>>()
+//{
+//    { 'a', new List<char> { '@' } },
+//    { 's', new List<char> { '5' } },
+//    { 'o', new List<char> { '0' } }
+//};
+
+//List<string> permutations = new List<string>() { "" };
+
+//foreach (char character in password)
+//{
+//    List<string> newPermutations = new List<string>();
+
+//    List<char> options = new List<char> { char.ToLower(character), char.ToUpper(character) };
+
+//    if (leetMap.ContainsKey(char.ToLower(character)))
+//    {
+//        options.AddRange(leetMap[char.ToLower(character)]);
+//    }
+
+//    foreach (string letter in permutations)
+//    {
+//        foreach (char option in options)
+//        {
+//            newPermutations.Add(letter + option);
+//        }
+//    }
+//    permutations = newPermutations;
+//}
+//string filePath = Path.Combine(Directory.GetCurrentDirectory(), "dict.txt");
+//File.WriteAllLines(filePath, permutations);
+
+//string[] lines = File.ReadAllLines(filePath);
+//List<string> fileContents = new List<string>(lines);
+
+using (HttpClient client = new HttpClient())
 {
-    { "a", "@" },
-    { "s", "5" },
-    { "o", "0" },
-};
+    HttpResponseMessage response = await client.GetAsync("https://jsonplaceholder.typicode.com/todos/1");
+    if (response.IsSuccessStatusCode)
+    {
+        string responseBody = await response.Content.ReadAsStringAsync();
+        Console.WriteLine(responseBody);
 
-List<string> permutations = new List<string>();
+    }
+}
 
-Console.WriteLine(special_chars["a"]);
+Console.WriteLine("JJs Nuts");
