6b14eabe52af51ab3d3e54ca844e56e8318d346e
Sins_621 <sinsmailza@gmail.com>
2025-04-18 15:20:58 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿string password = "Password";
+
+Dictionary<string, string> special_chars = new Dictionary<string, string>()
+{
+    { "a", "@" },
+    { "s", "5" },
+    { "o", "0" },
+};
+
+List<string> permutations = new List<string>();
+
+Console.WriteLine(special_chars["a"]);
