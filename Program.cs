9ed7f4524b4bba75a5a69a41ab6be0935037a237
Sins_621 <sinsmailza@gmail.com>
2025-04-19 15:29:49 +0200

    add/case-example

--- Program.cs
+++ Program.cs
@@ -1,6 +1,34 @@
-﻿using System.Runtime.CompilerServices;
+﻿/* === PREFACE ===
+Thank you for the opportunity to apply at Warp! 
 
-string password = "password";
+Initially I wanted to apply at a later time however JJ Williams mentioned to me
+that there is an urgent search for two developers to handle React related work
+and so he recommended that I apply at present. I had planned to learn C# at a 
+later stage prior to applying and as a result I wasn't expecting to complete 
+a C# assessment at this time and so if the following solution does not meet
+your standards I hope that there will be another opportunity in the future to
+make a second attempt when I am more familiar with the language.
+
+I raised this with him but he recommended that I make an attempt anyway and so
+using what I have learnt from other languages I will make my best effort to 
+complete the exercise at this time. The application mentions to attempt the
+exercise without the use of Google and Stack Overflow however unfortunately
+at present with my limited knowledge of the language that would not be feasible.
+
+To respect the rules of the assessment as well as I can I am limiting my internet
+search queries to "How to create a Dictionary in C#", in other words, mostly syntax
+related queries and not logic related queries. I hope you will understand.
+
+Thanks again,
+Bradly.
+*/
+
+
+using System.Runtime.CompilerServices;
+
+
+string password = "as5";
+
 
 Dictionary<char, List<char>> leetMap = new Dictionary<char, List<char>>()
 {
@@ -14,13 +42,22 @@ List<string> permutations = new List<string>() { "" };
 foreach (char character in password)
 {
     List<string> newPermutations = new List<string>();
+    List<char> options = new List<char>();
 
-    List<char> options = new List<char> { char.ToLower(character), char.ToUpper(character) };
-
-    if (leetMap.ContainsKey(char.ToLower(character)))
+    if (!char.IsLetter(character))
     {
-        options.AddRange(leetMap[char.ToLower(character)]);
+        options.Add(character);
     }
+    else
+    {
+        options.AddRange([char.ToLower(character), char.ToUpper(character)]);
+
+        if (leetMap.ContainsKey(char.ToLower(character)))
+        {
+            options.AddRange(leetMap[char.ToLower(character)]);
+        }
+    }
+
 
     foreach (string letter in permutations)
     {
@@ -31,25 +68,32 @@ foreach (char character in password)
     }
     permutations = newPermutations;
 }
-string filePath = Path.Combine(Directory.GetCurrentDirectory(), "dict.txt");
-File.WriteAllLines(filePath, permutations);
 
-string[] linesFromFile = File.ReadAllLines(filePath);
-List<string> passwords = new List<string>(linesFromFile);
-
-HttpClient client = new HttpClient();
-
-foreach (string word in passwords)
+foreach (string word in permutations)
 {
-
+    Console.WriteLine(word);
 }
 
-HttpResponseMessage response = await client.GetAsync("https://jsonplaceholder.typicode.com/todos/1");
 
-if (response.IsSuccessStatusCode)
-{
-    string responseBody = await response.Content.ReadAsStringAsync();
-    Console.WriteLine(responseBody);
+//string filePath = Path.Combine(Directory.GetCurrentDirectory(), "dict.txt");
+//File.WriteAllLines(filePath, permutations);
 
-}
+//string[] linesFromFile = File.ReadAllLines(filePath);
+//List<string> passwords = new List<string>(linesFromFile);
+
+//HttpClient client = new HttpClient();
+
+//foreach (string word in passwords)
+//{
+
+//}
+
+//HttpResponseMessage response = await client.GetAsync("https://jsonplaceholder.typicode.com/todos/1");
+
+//if (response.IsSuccessStatusCode)
+//{
+//    string responseBody = await response.Content.ReadAsStringAsync();
+//    Console.WriteLine(responseBody);
+
+//}
 
--- README.md
+++ README.md
@@ -1,11 +1,3 @@
-# Warp Development - Junior Fullstack Developer Assignment
+## C# Assessment
 
-This repository contains my submission for the Junior Fullstack Developer position at Warp Development. The role seeks developers proficient in both C# and React. While I was initially under the impression they were primarily seeking React developers at the time, I have approached this assignment using C# despite my limited experience with the language.
-
-## Background
-
-I was recommended for this position by JJ Williams, a current employee at Warp Development. I appreciate the opportunity to showcase my problem-solving abilities and willingness to step outside my comfort zone by tackling this challenge using C#.
-
-## Note
-
-Although my background is not primarily in C#, I am committed to delivering the best possible solution while learning and applying C# principles throughout this exercise.
+This repo holds the source code to a C# Assessment which is being used as an application at a Software Development Company in South Africa.
