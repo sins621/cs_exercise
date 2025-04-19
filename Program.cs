defa51e005ab62e76516fbe7c5d64731339f7036
Sins_621 <sinsmailza@gmail.com>
2025-04-19 15:45:04 +0200

    add/data-structure-explanation

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿/* === PREFACE ===
-Thank you for the opportunity to apply at Warp! 
+Thank you for the opportunity to apply! 
 
 Initially I wanted to apply at a later time however JJ Williams mentioned to me
 that there is an urgent search for two developers to handle React related work
@@ -27,8 +27,75 @@ Bradly.
 using System.Runtime.CompilerServices;
 
 
-string password = "as5";
+/* === 1. Password Generation ===
 
+
+I first attempted to solve the the problem of making case permuations as I
+had an idea for an iterative solution and figured I may find a way to adapt
+the solution to potentially adding leet characters later.
+
+string password = "ab";
+
+List<string> permutations = new List<string>() { "" };
+
+* Why did I choose the list data structure instead of just a string array?
+* Well, I googled "How to make a list in C#" because ever since learning
+* python I think of every array that doesn't use pointers as a list.
+* It turned out to be a happy accident as from what I gather in the docs
+* arrays are fixed and their size is not mutable at runtime.
+
+* permuations is initialized with an empty string so there is at least
+* one value to begin iterating over.
+
+foreach (char character in password)
+{
+    List<string> temp = new List<string>();
+    if (char.IsLetter(character))
+    {
+        foreach (string permutation in permutations)
+        {
+            temp.Add(permutation + char.ToLower(character));
+            temp.Add(permutation + char.ToUpper(character));
+        }
+    }
+    else
+    {
+        foreach (string permutation in permutations)
+        {
+            temp.Add(permutation + character);
+        }
+    }
+
+    permutations = temp;
+}
+
+* The core logic is to loop over each character in the password 
+* and use an inner loop to generate permutations by concatenating 
+* the two possibilites, in this case upper and lower case, to each 
+* existing permuation and saving it to a temporary list.
+* The permutation list will then updated with the temp list and 
+* be used in the following iterations with updated values to 
+* generate iterations for any length of the input string.
+
+* In this example "ab":
+* Step 1: a
+* permutations list starts with [""]
+* We add:
+* "" + "a" -> "a"
+* "" + "A" -> "A"
+* permuations becomes ["a", "A"]
+* Step 2: b
+* Now permutations is ["a", "A"]
+* For each one, we add:
+* "a" + "b" -> "ab"
+* "a" + "B" -> "aB"
+* "A" + "b" -> "Ab"
+* "A" + "B" -> "AB"
+* Result: ["ab", "aB", "Ab", "AB"]
+
+*/
+
+string password = "password";
 
 Dictionary<char, List<char>> leetMap = new Dictionary<char, List<char>>()
 {
@@ -36,6 +103,24 @@ Dictionary<char, List<char>> leetMap = new Dictionary<char, List<char>>()
     { 's', new List<char> { '5' } },
     { 'o', new List<char> { '0' } }
 };
+
+/*
+For the final implementation including leet characters I decided to use a 
+dictionary data structure for the variations of certain characters where 
+each key is the character that has variations and the values are a list of
+variations for that character. From what I've read online, dictionaries in C#
+also have extremely fast lookup times as they do in Python and so we will
+later be able to use it for the condition of "if the dictionary
+contains this character do x" without negatively impacting performance as
+the amount of variations grows. I think the use of a list for variations
+is okay for this solution as we are simply copying values from it and not
+performing any look ups in the algorithm.
+
+Why did I want to make the solution scalable? Well to be honest, I remember
+a seeing twitter post where the author shows multiple ways to solve the "FizzBuzz"
+interview question and in his final "best" solution he makes it scalable
+to accommodate multiple keywords and I wanted to do the same.
+*/
 
 List<string> permutations = new List<string>() { "" };
 
@@ -58,7 +143,6 @@ foreach (char character in password)
         }
     }
 
-
     foreach (string letter in permutations)
     {
         foreach (char option in options)
@@ -68,12 +152,6 @@ foreach (char character in password)
     }
     permutations = newPermutations;
 }
-
-foreach (string word in permutations)
-{
-    Console.WriteLine(word);
-}
-
 
 //string filePath = Path.Combine(Directory.GetCurrentDirectory(), "dict.txt");
 //File.WriteAllLines(filePath, permutations);
